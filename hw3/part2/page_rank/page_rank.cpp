70202c818b759056adeaff30eda87295d96dbfd7
yuhua99 <8709142+yuhua99@users.noreply.github.com>
2020-11-18 21:17:17 +0800

    [hw3] add pagerank

--- hw3/part2/page_rank/page_rank.cpp
+++ hw3/part2/page_rank/page_rank.cpp
@@ -17,43 +17,90 @@
 //
 void pageRank(Graph g, double *solution, double damping, double convergence)
 {
-
   // initialize vertex weights to uniform probability. Double
   // precision scores are used to avoid underflow for large graphs
 
   int numNodes = num_nodes(g);
   double equal_prob = 1.0 / numNodes;
+  // init solution value
   for (int i = 0; i < numNodes; ++i)
   {
     solution[i] = equal_prob;
   }
 
-  /*
-     For PP students: Implement the page rank algorithm here.  You
-     are expected to parallelize the algorithm using openMP.  Your
-     solution may need to allocate (and free) temporary arrays.
+  double *score_new = new double[numNodes];
+  double *score_out = new double[numNodes];
+  int *num_out = new int[numNodes];
+  int *num_in = new int[numNodes];
+  std::vector<int> end_ver;
 
-     Basic page rank pseudocode is provided below to get you started:
+#pragma omp parallel for schedule(guided)
+  for (int i = 0; i < numNodes; ++i)
+  {
+    num_out[i] = outgoing_size(g, i);
+    // if the vertax has no out edge
+    if (num_out[i] == 0)
+    {
+#pragma omp critical
+      end_ver.push_back(i);
+    }
+    num_in[i] = incoming_size(g, i);
+  }
 
-     // initialization: see example code above
-     score_old[vi] = 1/numNodes;
+  double glob_diff = convergence;
+  double add_up = (1.0 - damping) / numNodes;
 
-     while (!converged) {
+  // loop until converge
+  while (glob_diff >= convergence)
+  {
+    // compute out value
+#pragma omp parallel for
+    for (int i = 0; i < numNodes; ++i)
+    {
+      if (num_out[i] != 0)
+        score_out[i] = solution[i] / num_out[i];
+    }
+    // updating new score
+#pragma omp parallel for schedule(guided)
+    for (int i = 0; i < numNodes; ++i)
+    {
+      score_new[i] = 0;
+      for (int j = 0; j < num_in[i]; ++j)
+      {
+        score_new[i] += score_out[g->incoming_edges[g->incoming_starts[i] + j]];
+      }
+      score_new[i] = score_new[i] * damping + add_up;
+    }
 
-       // compute score_new[vi] for all nodes vi:
-       score_new[vi] = sum over all nodes vj reachable from incoming edges
-                          { score_old[vj] / number of edges leaving vj  }
-       score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
+    // add score from no out edge vertax
+    double s = 0;
+#pragma omp parallel for reduction(+ \
+                                   : s)
+    for (int i = 0; i < end_ver.size(); ++i)
+    {
+      s += damping * solution[end_ver[i]] / numNodes;
+    }
+#pragma omp parallel for
+    for (int i = 0; i < numNodes; ++i)
+    {
+      score_new[i] += s;
+    }
 
-       score_new[vi] += sum over all nodes v in graph with no outgoing edges
-                          { damping * score_old[v] / numNodes }
-
-       // compute how much per-node scores have changed
-       // quit once algorithm has converged
-
-       global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
-       converged = (global_diff < convergence)
-     }
-
-   */
+    // compute global difference
+    glob_diff = 0;
+#pragma omp parallel for reduction(+ \
+                                   : glob_diff)
+    for (int i = 0; i < numNodes; ++i)
+    {
+      glob_diff += abs(solution[i] - score_new[i]);
+    }
+    for (int i = 0; i < numNodes; ++i)
+    {
+      solution[i] = score_new[i];
+    }
+  }
+  delete[] score_new;
+  delete[] score_out;
+  delete[] num_out;
+  delete[] num_in;
 }
