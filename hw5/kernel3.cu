e1714267f9a84da9a986bb5d11a80ce462287dbd
yuhua99 <8709142+yuhua99@users.noreply.github.com>
2020-12-24 02:39:22 +0800

    [hw5] done kernel 1~3

---
+++ hw5/kernel3.cu
@@ -0,0 +1,75 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BlockSize 16
+#define GroupSize 4
+
+__global__ void mandelKernel(int *cudaMem, float lowerX, float lowerY,
+                             float stepX, float stepY, int maxIteration,
+                             int widthX, int widthY, int pitch)
+{
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+    int xid = (threadIdx.x + blockIdx.x * BlockSize) * GroupSize;
+    int yid = threadIdx.y + blockIdx.y * BlockSize;
+    if (yid >= widthY)
+        return;
+
+    float y = lowerY + Yid * stepY;
+    for (int i = 0; i < GroupSize; i++)
+    {
+        if (xid >= widhtX)
+            return;
+
+        float x = lowerX + xid * stepX;
+        float z_re = x, z_im = y;
+
+        int j;
+        for (j = 0; j < maxIteration; j++)
+        {
+            if (z_re * z_re + z_im * z_im > 4.f)
+                break;
+            float new_re = z_re * z_re - z_im * z_im;
+            float new_im = 2.f * z_re * z_im;
+            z_re = x + new_re;
+            z_im = y + new_im;
+        }
+        cudaMem[xid + yid * pitch] = j;
+        xid++;
+    }
+}
+
+// Host front-end function that allocates the memory and launches the GPU kernel
+void hostFE(float upperX, float upperY, float lowerX, float lowerY, int *img, int resX, int resY, int maxIterations)
+{
+    float stepX = (upperX - lowerX) / resX;
+    float stepY = (upperY - lowerY) / resY;
+
+    int *Mem;
+    cudaHostAlloc((void **)&Mem, resX * resY * sizeof(int), cudaHostAllocDefault);
+
+    int *cudaMem;
+    size_t pitch;
+    cudaMallocPitch((void **)&cudaMem, &pitch, rexX * sizeof(int), resY);
+    fflush(stdout);
+
+    dim3 dimBLock(BlockSize, BlockSize);
+    // group them
+    dim3 dimGrid((resX / BLockSize * GroupSize) +
+                     (resX % (BlockSize * GroupSize) == 0 ? 0 : 1),
+                 (resY / BLockSize) + (resY % BlockSize == 0 ? 0 : 1));
+    //  run on GPU
+    mandelKernel<<<dimGrid, dimBlock>>>(cudaMem, lowerX, lowerY, stepX, stepY,
+                                        maxIterations, resX, resY, pitch / sizeof(int));
+    //  wait for work done
+    cudaDeviceSynchronize();
+    cudaMemcpy2D((void *)Mem, resX * sizeof(int), (void *)cudaMem, pitch,
+                 resX * sizeof(int), resY, cudaMemcpyDeviceToHost);
+    memcpy((void *)img, (void *)Mem, resX * resY * sizeof(int));
+    // free mem
+    cudaFree(cudaMem);
+    cudaFreeHost(Mem);
+}
